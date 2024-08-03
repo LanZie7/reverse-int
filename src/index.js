740b114124c85c4cfc1c42c4336ec0042bedf939
bochkova_su <bochkova_su@resanta.ru>
2024-08-03 16:20:00 +0300

    number reversed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const string = Math.abs(n).toString();
+    const reversedString = parseInt(string.split('').reverse().join(''));
+    return reversedString;
 }
